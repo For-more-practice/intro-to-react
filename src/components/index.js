04f471b9233de710629b8e6f4544e4bde5d86967
justinmavity <justinmavity1021@gmail.com>
2021-10-11 08:52:29 -0500

    Add url and api key for axios calls

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+//Pokemon URL and API key for fetching data.
+export const Pokemon_BASE_URL = '';
+export const Pokemon_API_KEY = '';
+
+//Rick and Morty URL and API key for fetching data.
+export const RickAndMorty_BASE_URL = 'https://rickandmortyapi.com';
+export const RickAndMorty_API_KEY = '/api/character';
+
+
+//Star Wars URL and API key for fetching data.
+export const StarWars_BASE_URL = '';
+export const StarWars_API_KEY = '';
