a630ee42529c26fe748d9b38124f63f1ae391864
justinmavity <justinmavity1021@gmail.com>
2022-04-26 08:25:23 -0500

    Moved the links into another component name Home, now app.js is only doing the routing for the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import RickMortyHome from './components/RickAndMorty/RickMortyHome';
+import Home from './Home';
 import './App.css';
 
 export default function App() {
@@ -13,41 +14,14 @@ export default function App() {
 
   return (
     <div className="App">
-      <div className='cover-container'>
-        <div className='container'>
-          <Link to='/RickMorty'>
-            <div className='image-container'>
-              <div className='Rick-Morty-cover'>
-                <h2>Rick and Morty</h2>
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className='container'>
-          <Link to='/Pokemon'>
-            <div className='image-container'>
-              <div className='Pokemon-cover'>
-                <h2>Pokemon</h2>
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className='container'>
-          <Link to='/Starwars'>
-            <div className='image-container'>
-              <div className='Starwars-cover'>
-                <h2>StarWars</h2>
-              </div>
-
-            </div>
-          </Link>
-        </div>
-        <Switch>
-          <Route path='/RickMorty'>
-            <RickMortyHome />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/RickMorty'>
+          <RickMortyHome />
+        </Route>
+      </Switch>
     </div>
   );
 }
